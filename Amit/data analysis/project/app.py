82f81d710778e104b1a49ed19a018d82f15641eb
MrMazen101 <yehiamazen216@gmail.com>
2026-02-24 03:05:21 +0200

    test

--- Amit/data analysis/project/app.py
+++ Amit/data analysis/project/app.py
@@ -1,4 +1,4 @@
-
+pip install streamlit
 import streamlit as st
 import pandas as pd
 import seaborn as sns
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 st.set_page_config(page_title="Ford GoBike Dashboard", page_icon="🚲", layout="wide")
 
 st.title("🚲 Ford GoBike Data Exploration")
-st.write("أهلاً بيك في الداشبورد الخاصة بتحليل بيانات نظام مشاركة الدراجات!")
+st.write("hi! Welcome to the Ford GoBike data dashboard. Here you can explore the cleaned dataset and gain insights about bike usage patterns.")
 st.markdown("---")
 
 # 2. قراءة الداتا النظيفة
@@ -20,12 +20,12 @@ def load_data():
 df = load_data()
 
 # 3. عرض عينة من البيانات
-st.subheader("نظرة سريعة على البيانات (أول 5 صفوف)")
+st.subheader("take a look at a sample of the cleaned dataset")
 st.dataframe(df.head())
 st.markdown("---")
 
 # 4. أول رسمة تفاعلية (أنواع المستخدمين)
-st.subheader("توزيع المستخدمين حسب النوع (Subscriber vs Customer)")
+st.subheader("Distribution of User Types between Subscribers and Customers")
 fig, ax = plt.subplots(figsize=(8, 4))
 base_color = sns.color_palette()[0]
 sns.countplot(data=df, x='user_type', color=base_color, ax=ax)
